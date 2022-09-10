304b7ee16b93c4fe6672be91f3c9d9daf98510e2
Moustafa <moustafaism013@hotmail.com>
2022-09-10 13:43:49 +0300

    Added the queries for university database creation and tables creation

---
+++ universityCreation.sql
@@ -0,0 +1,75 @@
+CREATE SCHEMA IF NOT EXISTS `universitydb` DEFAULT CHARACTER SET utf8 ;
+USE `universitydb` ;
+-- -----------------------------------------------------
+-- Table `universitydb`.`students`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`students` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+-- -----------------------------------------------------
+-- Table `universitydb`.`departments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`departments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `office` VARCHAR(255) NULL,
+  PRIMARY KEY (`id`));
+-- -----------------------------------------------------
+-- Table `universitydb`.`rooms`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`rooms` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `capacity` INT NULL,
+  PRIMARY KEY (`id`));
+-- -----------------------------------------------------
+-- Table `universitydb`.`courses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`courses` (
+  `course_name` VARCHAR(255) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `start_time` TIME NULL,
+  `end_time` TIME NULL,
+  `room_id` INT NOT NULL,
+  PRIMARY KEY (`course_name`, `room_id`),
+  CONSTRAINT `fk_courses_rooms`
+    FOREIGN KEY (`room_id`)
+    REFERENCES `universitydb`.`rooms` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+-- -----------------------------------------------------
+-- Table `universitydb`.`majorsIn`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`majorsIn` (
+  `student_id` INT NOT NULL,
+  `department_id` INT NOT NULL,
+  PRIMARY KEY (`student_id`, `department_id`),
+  CONSTRAINT `fk_students_has_departments_students1`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `universitydb`.`students` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_students_has_departments_departments1`
+    FOREIGN KEY (`department_id`)
+    REFERENCES `universitydb`.`departments` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+-- -----------------------------------------------------
+-- Table `universitydb`.`enrolled`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `universitydb`.`enrolled` (
+  `course_name` VARCHAR(255) NOT NULL,
+  `student_id` INT NOT NULL,
+  `credit_status` VARCHAR(255) NULL,
+  PRIMARY KEY (`course_name`, `student_id`),
+  CONSTRAINT `fk_courses_has_students_courses1`
+    FOREIGN KEY (`course_name`)
+    REFERENCES `universitydb`.`courses` (`course_name`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_courses_has_students_students1`
+    FOREIGN KEY (`student_id`)
+    REFERENCES `universitydb`.`students` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
