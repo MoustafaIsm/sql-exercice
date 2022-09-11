afbedffaa9718183359eb53ab3bfa304115f2454
Moustafa <moustafaism013@hotmail.com>
2022-09-11 08:36:34 +0300

    Added universitydb question 4

--- universityRequiredQueries.sql
+++ universityRequiredQueries.sql
@@ -9,4 +9,6 @@ FROM courses c
 HAVING c.start_time = MIN(c.start_time)
 
 -- Question 4 
-
+SELECT c.name
+FROM departments d, majorsin m, enrolled e, courses c
+WHERE d.id = m.department_id AND m.student_id = e.student_id AND c.crn = e.crn AND d.name = "BIF"
