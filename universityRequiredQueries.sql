8a379f9c7f7e610cda7b2a08ec36f025820620d9
Moustafa <moustafaism013@hotmail.com>
2022-09-11 08:43:40 +0300

    Added universitydb question 5

--- universityRequiredQueries.sql
+++ universityRequiredQueries.sql
@@ -12,3 +12,13 @@ HAVING c.start_time = MIN(c.start_time)
 SELECT c.name
 FROM departments d, majorsin m, enrolled e, courses c
 WHERE d.id = m.department_id AND m.student_id = e.student_id AND c.crn = e.crn AND d.name = "BIF"
+
+-- Question 5
+SELECT name
+FROM students
+WHERE NOT EXISTS (
+    SELECT s.id
+    FROM students s, enrolled e
+    WHERE s.id = e.student_id)
+
+--Quetsion 6
